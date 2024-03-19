7a2614f5af4e8f52ee9fd63d0bfabf80b2a2a3dd
Samra17 <62614263+Samra17@users.noreply.github.com>
2024-03-19 16:20:20 +0100

    add: empty sping boot project

---
+++ tim3/src/main/java/com/nbp/tim3/Tim3Application.java
@@ -0,0 +1,13 @@
+package com.nbp.tim3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tim3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tim3Application.class, args);
+	}
+
+}

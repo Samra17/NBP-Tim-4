1b37a5d9400d23fe541ee50e159b9e871412bb7d
muharemkapo <muharem.kapo@evolt.dev>
2024-03-29 15:19:57 +0100

    Firebase bucket logic added

---
+++ tim3/src/main/java/com/nbp/tim3/service/FirebaseService.java
@@ -0,0 +1,68 @@
+package com.nbp.tim3.service;
+
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.nbp.tim3.exception.OrderPatchInvalidException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.UUID;
+
+@Service
+public class FirebaseService {
+
+    @Value("${BUCKET_NAME}")
+    private String bucketName;
+
+    public String upload(MultipartFile multipartFile) {
+        try {
+            String fileName = multipartFile.getOriginalFilename();
+            fileName = UUID.randomUUID().toString().concat(this.getExtension(fileName));
+
+            File file = this.convertToFile(multipartFile, fileName);
+            String URL = this.uploadFile(file, fileName);
+            file.delete();
+            return URL;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Image couldn't upload, Something went wrong");
+        }
+    }
+
+    private File convertToFile(MultipartFile multipartFile, String fileName) throws IOException {
+        File tempFile = new File(fileName);
+        try (FileOutputStream fos = new FileOutputStream(tempFile)) {
+            fos.write(multipartFile.getBytes());
+        }
+        return tempFile;
+    }
+
+    private String uploadFile(File file, String fileName) throws IOException {
+
+        BlobId blobId = BlobId.of(bucketName, fileName);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("media").build();
+        Credentials credentials = GoogleCredentials.getApplicationDefault();
+        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+        storage.create(blobInfo, Files.readAllBytes(file.toPath()));
+
+        String DOWNLOAD_URL = "https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media";
+        return String.format(DOWNLOAD_URL, URLEncoder.encode(bucketName, StandardCharsets.UTF_8), URLEncoder.encode(fileName, StandardCharsets.UTF_8));
+    }
+
+    private String getExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf("."));
+    }
+
+}
